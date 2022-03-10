dfe428dc289e1a7ca5662e8e38248cd8fa12b958
Josefine-Gm-Paaske <1086237@ucn.dk>
2022-03-10 16:56:31 +0100

    Updated files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,35 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true
+    es2021: true,
+    node: true
   },
+
   extends: [
     'plugin:vue/vue3-recommended',
   ],
+
   parserOptions: {
     ecmaVersion: 'latest',
-    sourceType: 'module'
+    sourceType: 'module',
+    parser: '@babel/eslint-parser'
   },
+
   plugins: [
     'vue'
   ],
+
   rules: {
-  }
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+
+  root: true,
+
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended'
+  ]
 }
