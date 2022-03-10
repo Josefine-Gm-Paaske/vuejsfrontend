058f52c6dbeb9fb44b1a6cc66b1d58990c1d9918
Josefine-Gm-Paaske <1086237@ucn.dk>
2022-03-10 16:52:15 +0100

    Updated app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <vue-js-front-end msg="Welcome to my Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import VueJsFrontEnd from './components/VueJsFrontEnd.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    VueJsFrontEnd
   }
 }
 </script>
